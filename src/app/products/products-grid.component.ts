8ce8d9f149f39efbd3916eda614c4ddca526676b
DESKTOP-QV9DMMO\GH <rdupra.globagility@gmail.com>
2021-06-12 10:10:08 +0800

    place src folder

---
+++ src/app/products/products-grid.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "products-table",
+  templateUrl: "products-grid.component.html",
+  styleUrls: [],
+  providers: [],
+})
+export class ProductsGridComponent {}
